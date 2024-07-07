67994914f8c59caaad7d739a00c029f80e9e6310
Ethanlyt <ethanleong86@gmail.com>
2024-07-07 15:17:08 +0800

    refactor: Update welcome message in Dashboard component

--- app/(desap)/login/page.tsx
+++ app/(desap)/login/page.tsx
@@ -40,6 +40,7 @@ export default function Login() {
 			password: data.password,
 			redirect: false,
 		});
+		console.log(signInData);
 		setIsLoading(false);
 		if (signInData?.error) {
 			toast({
--- app/shared/components/general-component/layout-component/Logo.tsx
+++ app/shared/components/general-component/layout-component/Logo.tsx
@@ -1,12 +1,15 @@
+"use client";
 import Image from "next/image";
 import DESAPLogo from "@/shared/image/logo.svg";
 import NextLink from 'next/link'
 import { Flex, Link } from "@chakra-ui/react";
+import { signOut, useSession } from "next-auth/react";
 
 export default function Logo (props: any) {
+	const { data: session } = useSession();
 	return (
 		<Flex alignItems='center'>
-			<Link as={NextLink} href='/landing'>
+			<Link as={NextLink} href={session === null ? "/" : "/landing"}>
 				<Image
 					priority
 					src={DESAPLogo}
